0341bbed26a6ed058696f6fb380363368d5982ad
Jean-Francis Bastien <bhacaz@gmail.com>
2016-02-03 15:33:37 -0500

    Ajout dossier Mechanical du robot, module Arduino et CameraTower

---
+++ Robot/Mechanical/Arduino.py
@@ -0,0 +1,8 @@
+from nanpy import ArduinoApi, SerialManager, Servo
+
+class Arduino:
+    def __init__(self):
+        connection = SerialManager(device='/dev/ttyUSB0')
+        self.a = ArduinoApi(connection=connection)
+
+    def initCameraTower(self):
