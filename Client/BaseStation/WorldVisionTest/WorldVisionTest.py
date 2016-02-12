b9a43d1984716585f182b4572f65451f404f9a13
phildupuis <dupuis_philippe@hotmail.ca>
2016-02-12 18:00:21 -0500

    Enlever import inutile

--- Client/BaseStation/WorldVisionTest/WorldVisionTest.py
+++ Client/BaseStation/WorldVisionTest/WorldVisionTest.py
@@ -1,12 +1,5 @@
 from unittest import TestCase
 
-from Client.BaseStation.Logic.Sequencer import Sequencer
-
-import Client.BaseStation.Logic.State.SendingBotToChargingStationState as SendingBotToChargingStationState
-import Client.BaseStation.Logic.State.SendingBotToTargetState as SendingBotToTargetState
-import Client.BaseStation.Logic.State.SendingBotToTreasureState
-
-
 class WorldVisionTest(TestCase):
     def test_givenAVisionTestWhenNothingThenNothing(self):
         return True
