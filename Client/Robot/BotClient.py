7c52221b16104b99e6323ac43dda84aaafdd4cbb
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2016-02-13 10:18:04 -0500

    Envoi de toutes les info relative aux robots (sauf la connection) dans un même package à tout les 5 secondes

--- Client/Robot/BotClient.py
+++ Client/Robot/BotClient.py
@@ -5,28 +5,28 @@ from Logic.RobotMock import RobotMock
 
 with open("../../Shared/config.json") as json_data_file:
     config = json.load(json_data_file)
-
-robot = RobotMock()
-
-
 socketIO = SocketIO(config['url'], int(config['port']))
 
+robot = RobotMock()
 def needNewCoordinates(*args):
     print("Bot going to " + args[0]["type"] + " at : (" + args[0]["position"]["positionX"] + " " + args[0]["position"]["positionY"] + ")")
     robot.moveTo((int(args[0]["position"]["positionX"]), int(args[0]["position"]["positionY"])))
     if(args[0]["type"] == "target"):
+        robot.botInfo = {"voltage" : "N/A",
+                        "decodedCharacter" : "N/A",
+                        "target" : "N/A"}
         socketIO.emit('endSignal')
     else:
-        robot.tension = "12V"
-        socketIO.emit('needNewCoordinates',{"decodedCharacter":"A", "target":"cercle"})
+        robot.botInfo['voltage'] = "12V"
+        robot.botInfo['decodedCharacter'] = "A"
+        robot.botInfo['target'] = "cercle"
+        socketIO.emit('needNewCoordinates')
 
-def sendVoltage():
-        socketIO.emit('sendingVoltage', robot.tension)
-
-
+def sendInfo():
+    socketIO.emit('sendingInfo', robot.botInfo)
 
 socketIO.emit('botClientStatus','Connected')
-socketIO.on('needUpdatedInfo', sendVoltage)
+socketIO.on('needUpdatedInfo', sendInfo)
 socketIO.on('sendingNextCoordinates', needNewCoordinates)
 
 socketIO.wait()
