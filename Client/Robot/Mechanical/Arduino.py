1f2a6b37ec4690993aef02dbdae8314b074e4f8a
Jean-Francis Bastien <bhacaz@gmail.com>
2016-02-23 16:54:44 -0500

    Refactoring

--- Client/Robot/Mechanical/Arduino.py
+++
@@ -1,9 +0,0 @@
-from nanpy import ArduinoApi, SerialManager, Servo
-
-class Arduino:
-    def __init__(self):
-        connection = SerialManager(device='/dev/ttyUSB0')
-        self.a = ArduinoApi(connection=connection)
-
-    def initCameraTower(self):
-        pass
