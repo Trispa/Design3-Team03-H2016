2e760554a4cb35de9a28100dd854a2bd55a376f8
phildupuis <dupuis_philippe@hotmail.ca>
2016-02-27 23:52:33 -0500

    Erreur suppresion de fichier

---
+++ Client/BaseStation/WorldVisionTest/mainForTest.py
@@ -0,0 +1,24 @@
+from Client.BaseStation.WorldVision.worldImage import WorldImage
+import cv2
+
+
+if __name__ == '__main__':
+
+    cap = cv2.VideoCapture(1)
+
+    while(True):
+        #ret, frame = cap.read()
+        frame = cv2.imread('Images/Test2.jpg')
+
+        mapImage = WorldImage(frame)
+        mapImage.setMap()
+        worldImage = mapImage.drawMapOnImage()
+
+        cv2.imwrite( "../../../Shared/worldImage.jpg", worldImage)
+        cv2.imshow('frame',worldImage)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
