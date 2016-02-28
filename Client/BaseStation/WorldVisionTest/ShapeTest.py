da03f90d6255ff954f030cd1ab22bb5b3c133958
phildupuis <dupuis_philippe@hotmail.ca>
2016-02-27 23:50:09 -0500

    Ajout Test Map

--- Client/BaseStation/WorldVisionTest/ShapeTest.py
+++ Client/BaseStation/WorldVisionTest/ShapeTest.py
@@ -6,10 +6,11 @@ import numpy as np
 class TestShape(TestCase):
 
     def test_givenTwoShapesWithSameContourWhenComparedThenTheyAreEqual(self):
-        myContour = np.array([[1,1],[10,50],[50,50]], dtype=np.int32)
+        myContour = np.array([[[1,1],[10,50],[50,50]]], dtype=np.int32)
         aGeometricalName = "Triangle"
         myFirstShape = Shape(aGeometricalName, myContour)
         mySecondShape = Shape(aGeometricalName, myContour)
+
         self.assertTrue(myFirstShape == mySecondShape)
 
     def test_givenTwoShapesWithDifferentContourWhenComparedThenTheyAreNotEqual(self):
@@ -18,6 +19,7 @@ class TestShape(TestCase):
         aGeometricalName = "Triangle"
         myFirstShape = Shape(aGeometricalName, myFirstContour)
         mySecondShape = Shape(aGeometricalName, mySecondContour)
+
         self.assertFalse(myFirstShape == mySecondShape)
 
     def test_givenTwoShapeWithOneNotIniTializeWhenComparedThenTheyAreNotEqual(self):
@@ -25,17 +27,20 @@ class TestShape(TestCase):
         aGeometricalName = "Triangle"
         myFirstShape = Shape(aGeometricalName, myFirstContour)
         mySecondShape = None
+
         self.assertFalse(myFirstShape == mySecondShape)
 
     def test_givenAShapeWithSimilarLenghtEdgesWhenIsEqualEdgesThenEdgesAreEquals(self):
         squareContour = np.array([[0,0], [0,1], [1,0], [1,1]], dtype=np.int32)
-        aGeometricalName = "Triangle"
+        aGeometricalName = "Square"
         myShape = Shape(aGeometricalName, squareContour)
+
         self.assertTrue(myShape.isEqualEdges())
 
     def test_givenAShapeWithDifferentLenghtEdgesWhenIsEqualEdgesThenEdgesAreNotEquals(self):
         squareContour = np.array([[0,0], [0,1], [1,30]], dtype=np.int32)
-        aGeometricalName = "Triangle"
+        aGeometricalName = "Square"
         myShape = Shape(aGeometricalName, squareContour)
+
         self.assertFalse(myShape.isEqualEdges())
 
--- Client/BaseStation/WorldVisionTest/mapBuilderTest.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-from Client.BaseStation.WorldVision.mapBuilder import MapBuilder
-import numpy as np
-
-class TestMapBuilder(TestCase):
-    pass
