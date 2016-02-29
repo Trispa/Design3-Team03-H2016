ce1a808614b1fdbcea9c5cf4cbd286005b267f95
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2016-02-28 19:11:33 -0500

    fichier désuet

--- Base/Logic/State/SendingBotToTreasureState.py
+++
@@ -1,6 +0,0 @@
-import SendingBotToTargetState
-
-class SendingBotToTreasureState():
-    def handle(self, context):
-        #call to pathfinder to return path to treasure
-        context.setState(SendingBotToTargetState.SendingBotToTargetState())
