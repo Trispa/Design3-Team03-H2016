8674d169bbde34c7ed19fb4b05b5ed5ecda4daa4
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2016-03-01 20:06:53 -0500

    defuk is going on commit

---
+++ Client/BaseStation/LogicTest/StateTest/SendingBotToTargetStateTest.py
@@ -0,0 +1,72 @@
+from unittest import TestCase
+from mock import MagicMock
+import sys
+
+
+from Client.BaseStation.Logic.State.SendingBotToTargetState import SendingBotToTargetState
+
+
+class SendingBotToTargetStateTest(TestCase):
+
+    def test_whenCreatingSaidStateThenObstacleIndexIs0AndPathIsSet(self):
+        testedState = SendingBotToTargetState()
+
+        self.assertEqual(testedState.obstacleIndex, 0)
+        self.assertIsNotNone(testedState.path)
+
+    def test_whenCallingHandleOnSaidStateThenReturnCoordinatesWithChargingStationType(self):
+        testedState = SendingBotToTargetState()
+        sequencer = MagicMock()
+
+        coordinates = testedState.handle(sequencer, 0)
+
+        self.assertEquals(coordinates["type"], "treasure")
+
+    def test_whenCallingHandleOnSaidStateWithThirdItemIndexThenReturnCoordinatesWithPositionTOCorrespondingAtTheCorrectCoordinatesFromPathList(self):
+        testedState = SendingBotToTargetState()
+        sequencer = MagicMock()
+
+        coordinates = testedState.handle(sequencer, 0)
+
+        self.assertEquals(coordinates["positionTO"]["positionX"], str(testedState.path[0][0]))
+        self.assertEquals(coordinates["positionTO"]["positionY"], str(testedState.path[0][1]))
+
+    def test_whenCallingHandleOnSaidStateWithLastItemThenSequencerIsCalledToChangeState(self):
+        testedState = SendingBotToTargetState()
+        sequencer = MagicMock()
+
+        testedState.handle(sequencer, testedState.path.__len__() - 1)
+
+        assert sequencer.setState.called
+
+    def test_whenCallingHandleOnSaidStateWithFirstItemThenSequencerIsNotCalledToChangeState(self):
+        testedState = SendingBotToTargetState()
+        sequencer = MagicMock()
+
+        testedState.handle(sequencer, 0)
+
+        assert not sequencer.setState.called
+
+    def test_whenCallingHandleOnSaidStateWithLastItemThenReturnedCoordinatesHasEndToNo(self):
+        testedState = SendingBotToTargetState()
+        sequencer = MagicMock()
+
+        coordinates = testedState.handle(sequencer, testedState.path.__len__() - 1)
+
+        self.assertEquals(coordinates["end"], "no")
+
+    def test_whenCallingHandleOnSaidStateWithLastItemThenReturnedCoordinatesHasIndexAtMinus1(self):
+        testedState = SendingBotToTargetState()
+        sequencer = MagicMock()
+
+        coordinates = testedState.handle(sequencer, testedState.path.__len__() - 1)
+
+        self.assertEquals(coordinates["index"], "-1")
+
+    def test_whenCallingHandleOnSaidStateWithAnyItemThenReturnedCoordinatesHasIndexAtSameIndex(self):
+        testedState = SendingBotToTargetState()
+        sequencer = MagicMock()
+
+        coordinates = testedState.handle(sequencer, 0)
+
+        self.assertEquals(coordinates["index"], "0")
