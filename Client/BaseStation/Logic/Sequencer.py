e188d278262097be5780cd7d122d0b2162b17806
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2016-03-01 20:36:22 -0500

    lol

--- Client/BaseStation/Logic/Sequencer.py
+++ Client/BaseStation/Logic/Sequencer.py
@@ -5,11 +5,7 @@ class Sequencer :
     def setState(self, newState) :
         self.state = newState
 
-    def visionCallToGetObstacleList(self):
-        return "whatever"
-
     def __init__(self) :
-        self.obstacleList = self.visionCallToGetObstacleList()
         self.setState(SequencerState.SendingBotToChargingStationState())
 
 
