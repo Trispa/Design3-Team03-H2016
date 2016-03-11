9f389fbc655e3f5221a4ed3103a56fc350636a9a
phildupuis <dupuis_philippe@hotmail.ca>
2016-03-11 10:41:40 -0500

    RefactworldVision et ajout resolution

--- Client/BaseStation/WorldVision/worldVision.py
+++ Client/BaseStation/WorldVision/worldVision.py
@@ -6,17 +6,18 @@ import cv2
 class worldVision:
 
     def __init__(self):
-        self.camera = cv2.VideoCapture(2)
-
+        self.camera = cv2.VideoCapture(1)
+        self.camera.set(3, 720)
+        self.camera.set(4, 720)
+        ret, frame = self.camera.read()
+        self.mapImage = WorldImage(frame)
 
     def getCurrentImage(self):
 
         ret, frame = self.camera.read()
-        geometricalImage = WorldImage(frame)
-        geometricalImage.setMap()
-        geometricalImage.addLabels()
-
-        worldImage = geometricalImage.drawMapOnImage()
+        self.mapImage.setMap(frame)
+        self.mapImage.addLabels(frame)
+        worldImage = self.mapImage.drawMapOnImage(frame)
 
         return worldImage
 
