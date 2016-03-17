4dd99498ac979ea00a4a1c92cf2f8cab1ca621a4
phildupuis <dupuis_philippe@hotmail.ca>
2016-03-17 19:45:57 -0400

    Iteration vision

--- Client/BaseStation/WorldVision/mapBuilder.py
+++ Client/BaseStation/WorldVision/mapBuilder.py
@@ -1,8 +1,17 @@
 import cv2
 import map
+import numpy as np
 from Factories.ShapeFactory import ShapeFactory
+from Factories.ColorFactory import ColorFactory
 
 class MapBuilder:
+
+    colorFactory = ColorFactory()
+    colors = []
+    colors.append(colorFactory.constructColor(np.uint8([[[0,255,0]]]), "Green"))
+    colors.append(colorFactory.constructColor(np.uint8([[[255,0,0]]]), "Blue"))
+    colors.append(colorFactory.constructColor(np.uint8([[[150,179,255]]]), "Red"))
+    colors.append(colorFactory.constructColor(np.uint8([[[0,255,255]]]), "Yellow"))
 
     def __init__(self):
         self.__map = map.Map()
@@ -44,7 +53,51 @@ class MapBuilder:
                         if len(contour) == 4:
                             map.setMapLimit(contour)
 
+        self.buildByColorClosing(mapImage, map)
+        self.buildByColorOpening(mapImage, map)
         map.setShapesColor(mapImage)
         map.filterRobot()
         map.deleteBlackShapes()
+        return map
+
+    def buildByColorClosing(self, mapImage, map):
+
+        for color in self.colors:
+            hsvImage = cv2.cvtColor(mapImage,cv2.COLOR_BGR2HSV)
+            coloredImage = cv2.inRange(hsvImage,color.lower,color.higher)
+
+            kernel = np.ones((5,5),np.uint8)
+            closing = cv2.morphologyEx(coloredImage, cv2.MORPH_CLOSE, kernel)
+
+            contours, hierarchy = cv2.findContours(closing, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            for contour in contours:
+                cnt_len = cv2.arcLength(contour, True)
+                contour = cv2.approxPolyDP(contour, 0.02*cnt_len, True)
+                if cv2.isContourConvex(contour) and cv2.contourArea(contour) > 100:
+                    myShape = self.shapeFactory.ConstructShape(contour)
+
+                    if myShape.isEqualEdges() and myShape.checkAngleValue():
+                        map.addShape(myShape)
+
+        return map
+
+    def buildByColorOpening(self, mapImage, map):
+
+        for color in self.colors:
+            hsvImage = cv2.cvtColor(mapImage,cv2.COLOR_BGR2HSV)
+            coloredImage = cv2.inRange(hsvImage,color.lower,color.higher)
+
+            kernel = np.ones((5,5),np.uint8)
+            closing = cv2.morphologyEx(coloredImage, cv2.MORPH_OPEN, kernel)
+
+            contours, hierarchy = cv2.findContours(closing, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            for contour in contours:
+                cnt_len = cv2.arcLength(contour, True)
+                contour = cv2.approxPolyDP(contour, 0.02*cnt_len, True)
+                if cv2.isContourConvex(contour) and cv2.contourArea(contour) > 100:
+                    myShape = self.shapeFactory.ConstructShape(contour)
+
+                    if myShape.isEqualEdges() and myShape.checkAngleValue():
+                        map.addShape(myShape)
+
         return map
--- Client/BaseStation/WorldVisionTest/Photo-Test/shapeSelector.py
+++ Client/BaseStation/WorldVisionTest/Photo-Test/shapeSelector.py
@@ -10,8 +10,8 @@ def printPosition(event,x,y,flags,param):
         resultFile.write("("+str(x)+","+str(y)+")")
         resultFile.close()
 
-beginingPhoto = 105
-endPhoto = 106
+beginingPhoto = 177
+endPhoto = 178
 
 for i in range(beginingPhoto,endPhoto+1):
     pictureNumber = i
