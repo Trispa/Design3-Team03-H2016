362f0f63b00d392e416c71b1af8929a1ab2674a5
gabsima <gabriel.simard.4@ulaval.ca>
2016-03-22 13:48:54 -0400

    Revert "Manchester"

--- Client/Robot/Mechanical/Arduino/pid_control/ReadManchester.cpp
+++ Client/Robot/Mechanical/Arduino/pid_control/ReadManchester.cpp
@@ -7,105 +7,67 @@
 #include "Arduino.h"
 #include "ReadManchester.h"
 
-/*********DEBUT********/
-volatile int oldVal = 0;
-volatile int newVal = 0;
-volatile unsigned long now = 0;
-unsigned long timeToChange = 0;
-unsigned long timebetweenNowToChange = 0;
-char bits[128];
-char* chainecopie = '\0';
-int indice = 0;
-/*********** FIN******/
-
 volatile unsigned int bitAllowed = 0;
-//volatile unsigned int  indice = 0;
+volatile unsigned int  indice = 0;
 volatile unsigned int data ; 
+char chaine[64];
+char* chainecopie = '\0';
 
-//int volatile trouve = 0;
+int volatile trouve = 0;
 void ReadManchester::readBitInterrupt(){
-  
-  now = micros();
 	bitAllowed = 1;
-  
-  
+	//delayMicroseconds(50);
 	
 }
-ReadManchester::ReadManchester(int pinManchester){
+ReadManchester::ReadManchester(int pinManchester, int pinClock)
+{
 	
 	pinMode(pinManchester, INPUT);
-	this->_pinManchester = pinManchester;
- 	this->enableInterrupt(true);	
-  this->enableManchester();
-  
+	pinMode(pinClock, INPUT);
+	_pinManchester = pinManchester;
+	_pinClock = pinClock;
+	enableInterrupt(true);	
+}
+
+char ReadManchester::booleanToChar(boolean b)
+{
+	
+	return (b)?'1':'0';
 }
 
 
-void   ReadManchester::getMaschesterBits()
+char*  ReadManchester::getMaschesterBits()
 {	
-	  if(this->_enableManchester){
-  		if(bitAllowed){
-      //Serial.println(now);
-      delayMicroseconds(14);
-      newVal = digitalRead(_pinManchester);
-      digitalWrite(13, newVal);
-      
-      timebetweenNowToChange = now - timeToChange;
-      //Serial.println(timebetweenNowToChange, DEC);
-      if(oldVal == HIGH){
-        if(timebetweenNowToChange > 40 && timebetweenNowToChange < 80 ){ // on vise envirion 44{
-          bits[indice++]= '1';
-        }else if (timebetweenNowToChange > 150 && timebetweenNowToChange < 175){ //on vise environ 112
-          bits[indice++]= '1';
-          bits[indice++]= '1';
-        }
-      }else if (oldVal == LOW){
-          if (timebetweenNowToChange > 100 && timebetweenNowToChange < 140){ //on vise environ 148
-               bits[indice++]= '0';
-          }else if (timebetweenNowToChange > 200 && timebetweenNowToChange < 250){ //on vise environ 212
-               bits[indice++]= '0';
-               bits[indice++]= '0';
-          }
-          
-      }
-      
-      if(indice > 128){
-        this->_chaineCopie = bits;
-        indice = 0;
-        this->enableInterrupt(false); 
-        this->disableManchester();
-     
-        
-      }
-     
-      timeToChange = now;
-      oldVal = newVal;
-      bitAllowed = 0;
-    }
- 
-	 }
-	 
-}
-
-char* ReadManchester::getChaineCopie(){
-  return this->_chaineCopie;
-  
+	
+		if(bitAllowed)
+		{     
+			data =  digitalRead(_pinManchester);
+			chaine[indice] = booleanToChar(data);
+			
+			indice++;
+			if(indice == 64)
+			  {
+				indice = 0 ;
+				chainecopie = chaine;
+				Serial.println(chaine);
+				enableInterrupt(false);	
+				
+					
+			  }
+			  
+			bitAllowed = 0;
+			
+		}
+	return chainecopie;	
 }
 
 void ReadManchester::enableInterrupt(boolean ansewer){
 		
 		if(ansewer)
-			attachInterrupt(digitalPinToInterrupt(_pinManchester), readBitInterrupt, CHANGE); 
+			attachInterrupt(digitalPinToInterrupt(_pinClock), readBitInterrupt, RISING); 
 		else
-			detachInterrupt(digitalPinToInterrupt(_pinManchester));
+			detachInterrupt(digitalPinToInterrupt(_pinClock));
 
 }
 
-void ReadManchester::disableManchester(){
-  this->_enableManchester = false;
-}
-void ReadManchester::enableManchester(){
-  this->_enableManchester = true;
-  this->enableInterrupt(true);
-}
 
--- Client/Robot/Mechanical/SerialPortCommunicator.py
+++ Client/Robot/Mechanical/SerialPortCommunicator.py
@@ -1,10 +1,7 @@
 import serial
-import binascii
-import re
 from time import sleep
 import struct
-from collections import Counter
-import MoteurRoue
+import binascii
 
 class SerialPortCommunicator:
     COMMAND_INDICATOR = "C"
@@ -12,17 +9,14 @@ class SerialPortCommunicator:
     TRUE = 1
     ONE_SECOND_DELAY = 1
     ONE_MINUTE_DELAY = 60
-    FIVE_SECOND_DELAY = 5
-    READ_CODE_MANCHESTER = 4
-    GET_CODE_MANCHESTER = 6
+
     LED_FUNCTION_ON = 1
     LED_FUNCTION_OFF = 2
     CHANGE_MOTEUR_SPEED = 3
+    GET_CODE_MANCHESTER = 4
     STOP_ALL_MOTEUR = 5
     CW = 0
     CCW = 1
-
-    ascii = []
 
 
     # def __init__(self, bitrateArduino = 9600, arduinoPort = "/dev/ttyUSB0"):
@@ -31,7 +25,7 @@ class SerialPortCommunicator:
     #     CCW = 1
 
 #Pololu : /dev/serial/by-id/pci-Pololu_Corporation_Pololu_Micro_Maestro_6-Servo_Controller_00021864-if0
-    def __init__(self, bitrateArduino = 115200, arduinoPort = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A7007dag-if00-port0"):
+    def __init__(self, bitrateArduino = 115200, arduinoPort = "/dev/serial/by-id/pci-FTDI_FT232R_USB_UART_A7007dag-if00-port0"):
         self.arduino = serial.Serial(arduinoPort, bitrateArduino, timeout = 1)
         #self.polulu = serial.Serial(poluluPort, bitratePolulu, timeout = 1)
         sleep(1)
@@ -78,71 +72,30 @@ class SerialPortCommunicator:
     def stopAllMotor(self):
         self._sendCommand(self.STOP_ALL_MOTEUR, self.FALSE, self.ONE_SECOND_DELAY, 1)
 
-######################manchester##################
-
-    def readManchesterCode(self):
-        self._sendCommand(self.READ_CODE_MANCHESTER,self.FALSE,self.ONE_SECOND_DELAY, 1)
-
-
+     #################################### MANCHESTER ################################
     def getManchesterCode(self):
-       return  self._sendCommand(self.GET_CODE_MANCHESTER,self.TRUE, self.ONE_SECOND_DELAY, 1)
-
-    def manchester_decode(self, chaine):
-        i = 0
-        chaine_paire = ""
-        chaine_impaire = ""
-        patern = "111111110"
-        for  indice  in range(0, len(chaine)):
-            if((indice % 2) == 0):
-                chaine_paire += chaine[indice]
-            else :
-                chaine_impaire += chaine[indice]
-
-        if(chaine_paire.find(patern) != -1):
-            return chaine_paire
-        else:
-            if(chaine_impaire.find(patern) != -1):
-
-                return chaine_impaire
-            else:
-                print ("le patern 111111110 est pas bien choisi")
-        return -2
-
+        return self._sendCommand(self.GET_CODE_MANCHESTER,self.TRUE,self.ONE_SECOND_DELAY, 1)
 
     def getCodebits(self):
         trouve = 0
         indice = 0
-        self.readManchesterCode()
-        sleep(1)
-        bits = self.getManchesterCode()
-        chaine = self.manchester_decode(bits)
-        if(chaine == -2):
-            return -2
-
+        chaine = self.getManchesterCode();
         c = ''
         data = ""
         patern = "111111110"
         if(chaine != ""):
-            index = chaine.index(patern)
-            print("index", index)
-            #if(index >= 7):
-            #indexTemp = index - 7
-            data =  chaine[index + 9 : index + 16]
-            #else:
-                #data =  chaine[index + 9 : index + 15]
-            # while  (trouve == 0):
-            #     indice  = indice + 1
-            #     bitStop = chaine[indice: indice+16]
-            #     if(bitStop[:9] == patern):
-            #         trouve == 1
-            #         data = bitStop[9:]
-            #         break
+            while  (trouve == 0):
+                indice  = indice + 1
+                bitStop = chaine[indice: indice+16]
+                if(bitStop[:9] == patern):
+                    trouve == 1
+                    data = bitStop[9:]
+                    break
         else:
             return -1
-        #print("chaine recu : "+chaine)
+        print("chaine recu : "+chaine)
         print("code  : " + data)
-        return data[:: -1]
-
+        return data
 
     def letter_from_bits(self,bits, encoding='utf-8', errors='surrogatepass'):
         n = int(bits, 2)
@@ -153,37 +106,23 @@ class SerialPortCommunicator:
         n = len(hex_string)
         return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
 
-    def foundCodesManchester(self):
-        nbr = 0
-        while(nbr< 10 ):
-            data = self.getCodebits()
-            if(data == -2):
-
-                print "erreur"
-                return -2
-            else:
-                lettre  = self.letter_from_bits(data)
-                self.ascii.append(lettre)
-            nbr = nbr+1
-        return self.ascii
-
     def getAsciiManchester(self):
-        self.foundCodesManchester()
-        word_counts = Counter(self.ascii)
-        top_tree = word_counts.most_common(1)
-        b = [str(i[0]) for i in top_tree]
-        print top_tree
-        return  b[0]
-
-### Utilisation du code Manchester########
+        data = self.getCodebits()
+        if(data == -1):
+            print ("la chaine recu est vide ")
+        else:
+            return spc.letter_from_bits(data)
+        return -2
+        #################################### END MANCHESTER ################################
 
 if __name__ == "__main__":
     spc = SerialPortCommunicator()
+    letter = spc.getAsciiManchester()
+    if(letter ==-2):
+        print("Erreur")
+    else:
+        print("ASCII :" + letter)
+    spc.stopAllMotor()
 
-    print spc.getAsciiManchester()
+    print(spc.getAsciiManchester())
 
-    #chaine = spc.getAsciiManchester()
-    #if( chaine == -2):
-        #print("ERREUR !")
-    #else:
-         #print("ASCII :" + chaine)
