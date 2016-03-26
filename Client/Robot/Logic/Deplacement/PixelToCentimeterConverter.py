ad7a4d1d16303df131a2f5e88bcd036b0ce6e3f9
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2016-03-26 13:25:43 -0400

    optimisation du ratio

--- Client/Robot/Logic/Deplacement/PixelToCentimeterConverter.py
+++ Client/Robot/Logic/Deplacement/PixelToCentimeterConverter.py
@@ -1,5 +1,5 @@
 class PixelToCentimeterConverter:
-    RATIO = (45/9.5)
+    RATIO = (50/9.5)
 
     def convertPixelToCentimeter(self, pointToBeConverted):
         pointConverted = (float(pointToBeConverted[0])/self.RATIO, float(pointToBeConverted[1])/self.RATIO)
