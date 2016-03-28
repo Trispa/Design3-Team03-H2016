1eb46820c6e9f5eb51a5c950fbfa0e9ecd8c0d89
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2016-03-28 11:49:33 -0400

    pl correction

--- Client/BaseStation/Logic/Pathfinding/MapAdaptator.py
+++ Client/BaseStation/Logic/Pathfinding/MapAdaptator.py
@@ -14,10 +14,11 @@ class MapAdaptator:
         for compteur in range(0, self.map.getShapesList().__len__()):
             currentShape = self.map.getShapesList()[compteur]
             centerX, centerY = currentShape.findCenterOfMass()
-            if centerX-minCorner[0] == obstaclesList[compteur-1].positionX:
-                centerX += 1
-            elif centerX-minCorner[0] < obstaclesList[compteur-1].positionX:
-                centerX += 2
+            if compteur > 0:
+                if centerX-minCorner[0] == obstaclesList[compteur-1].positionX:
+                    centerX += 1
+                elif centerX-minCorner[0] < obstaclesList[compteur-1].positionX:
+                    centerX += 2
             obstaclesList.append(Obstacle((centerX - minCorner[0],centerY - minCorner[1])))
         return obstaclesList, mapSizeX, mapSizeY, minCorner
 
