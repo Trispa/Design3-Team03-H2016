72db5a01b8d812558fc7dcb8ebad80b7c4ff7859
Jean-Francis Bastien <bhacaz@gmail.com>
2016-03-29 12:04:22 -0400

    changment de la fonction de rotation du robot et de langle de la camera

--- Client/Robot/Mechanical/GrabTresor.py
+++ Client/Robot/Mechanical/GrabTresor.py
@@ -4,3 +4,13 @@ class GrabTresor:
     def __init__(self, spc, maestro):
         self.maestro = maestro
         self.spc = spc
+
+        self.maestro.setSpeed(2, 15)
+
+
+    def setPositionToTakeTresor(self):
+        self.maestro.setTarget(2, 762.5 * 4)
+
+    def setPositionToSecuriseTresor(self):
+        self.maestro.setTarget(2, 1764.25 * 4)
+
