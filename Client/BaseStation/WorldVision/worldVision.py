726590de6ca50d2bae05f2c88db4a2db43386c5f
phildupuis <dupuis_philippe@hotmail.ca>
2016-03-30 13:56:22 -0400

    Refactor

--- Client/BaseStation/WorldVision/worldVision.py
+++ Client/BaseStation/WorldVision/worldVision.py
@@ -63,5 +63,5 @@ class worldVision:
         self.mapImage.defineTreasures(relativeAngles)
 
     def findBestTresorPosition(self):
-        self.mapImage.findBestTresor()
+        return self.mapImage.findBestTresor()
 
