702e6b4e60acbf8d16af81934c9162e1830e11ae
phildupuis <dupuis_philippe@hotmail.ca>
2016-03-30 13:53:19 -0400

    Position et orientation pour trouver tresor

--- Client/BaseStation/Logic/MapCoordinatesAjuster.py
+++ Client/BaseStation/Logic/MapCoordinatesAjuster.py
@@ -4,3 +4,6 @@ class MapCoordinatesAjuster:
 
     def convertPoint(self, pointToBeConverted):
         return (pointToBeConverted[0] - self.minCorner[0], pointToBeConverted[1] - self.minCorner[1])
+
+    def inverseConversion(self, pointToBeConverted):
+        return (pointToBeConverted[0] + self.minCorner[0], pointToBeConverted[1] + self.minCorner[1])
