812a691b800ad2e388c96ae86aca7b7aeaa65683
phildupuis <dupuis_philippe@hotmail.ca>
2016-04-03 19:10:35 -0400

    Tests passent

--- Client/BaseStation/WorldVisionTest/mapTest.py
+++ Client/BaseStation/WorldVisionTest/mapTest.py
@@ -1,19 +1,20 @@
 from unittest import TestCase
 from Client.BaseStation.WorldVision.map import Map
 from Client.BaseStation.WorldVision.shape import Shape
+from Client.BaseStation.WorldVision.allShapes import Square
 import numpy as np
 
 class TestMap(TestCase):
 
     def setUp(self):
         geometricalName = "Square"
-        firstContour = np.array([[[1,1], [1,2], [2,2], [2, 1]]], dtype=np.int32)
-        secondContour = np.array([[[0,0], [2,3], [3,3], [3, 2]]], dtype=np.int32)
-        differentContour = np.array([[[24,32], [34,32], [34,55], [24, 55]]], dtype=np.int32)
+        firstContour = np.array([[[1,1]], [[1,2]], [[2,2]], [[2, 1]]], dtype=np.int32)
+        secondContour = np.array([[[0,0]], [[2,3]], [[3,3]], [[3, 2]]], dtype=np.int32)
+        differentContour = np.array([[[124,132]], [[134,132]], [[134,155]], [[124, 155]]], dtype=np.int32)
 
-        self.firstShape = Shape(geometricalName, firstContour)
-        self.secondShape = Shape(geometricalName, secondContour)
-        self.differentShape = Shape(geometricalName, differentContour)
+        self.firstShape = Square(geometricalName, firstContour)
+        self.secondShape = Square(geometricalName, secondContour)
+        self.differentShape = Square(geometricalName, differentContour)
 
 
     def test_givenAMapContainingOneShapeAndSimilarShapeWhenFindingSimilarShapeInMapWithTheSimilarShapeThenReturnTheShapeContainedInMap(self):
@@ -30,3 +31,4 @@ class TestMap(TestCase):
 
         similarShapeInMap = myMap.findSimilarShape(self.differentShape)
         self.assertEqual(None, similarShapeInMap)
+
